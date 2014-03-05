15834d13fad5a2edea885beb1d7102f05511363e
Clinton Gormley <develop@traveljury.com>
2014-03-05 15:23:51 +0000

    Import of DRTECH/Search-Elasticsearch-1.10 from CPAN.
    gitpan-cpan-distribution: Search-Elasticsearch
    gitpan-cpan-version:      1.10
    gitpan-cpan-path:         DRTECH/Search-Elasticsearch-1.10.tar.gz
    gitpan-cpan-author:       DRTECH
    gitpan-cpan-maturity:     released

---
+++ lib/Search/Elasticsearch/Cxn/LWP.pm
@@ -0,0 +1,239 @@
+package Search::Elasticsearch::Cxn::LWP;
+$Search::Elasticsearch::Cxn::LWP::VERSION = '1.10';
+use Moo;
+with 'Search::Elasticsearch::Role::Cxn::HTTP',
+    'Search::Elasticsearch::Role::Cxn',
+    'Search::Elasticsearch::Role::Is_Sync';
+
+use LWP::UserAgent();
+use HTTP::Headers();
+use HTTP::Request();
+
+my $Cxn_Error = qr/
+            Can't.connect
+          | Server.closed.connection
+          | Connection.refused
+            /x;
+
+use namespace::clean;
+
+#===================================
+sub perform_request {
+#===================================
+    my ( $self, $params ) = @_;
+    my $uri    = $self->build_uri($params);
+    my $method = $params->{method};
+
+    my $request = HTTP::Request->new(
+        $method => $uri,
+        [   'Content-Type' => $params->{mime_type},
+            %{ $self->default_headers },
+        ],
+        $params->{data}
+    );
+
+    my $ua = $self->handle;
+    my $timeout = $params->{timeout} || $self->request_timeout;
+    if ( $timeout ne $ua->timeout ) {
+        $ua->conn_cache->drop;
+        $ua->timeout($timeout);
+    }
+    my $response = $ua->request($request);
+
+    return $self->process_response(
+        $params,               # request
+        $response->code,       # code
+        $response->message,    # msg
+        $response->content,    # body
+        $response->headers     # headers
+    );
+}
+
+#===================================
+sub error_from_text {
+#===================================
+    local $_ = $_[2];
+
+    return
+          /read timeout/                           ? 'Timeout'
+        : /write failed: Connection reset by peer/ ? 'ContentLength'
+        : /$Cxn_Error/                             ? 'Cxn'
+        :                                            'Request';
+}
+
+#===================================
+sub _build_handle {
+#===================================
+    my $self = shift;
+    my %args = (
+        keep_alive => 1,
+        parse_head => 0
+    );
+    if ( $self->is_https ) {
+        $args{ssl_opts} = { verify_hostname => 0 };
+    }
+    return LWP::UserAgent->new( %args, %{ $self->handle_args } );
+}
+
+1;
+
+# ABSTRACT: A Cxn implementation which uses LWP
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Search::Elasticsearch::Cxn::LWP - A Cxn implementation which uses LWP
+
+=head1 VERSION
+
+version 1.10
+
+=head1 DESCRIPTION
+
+Provides an HTTP Cxn class and based on L<LWP>.
+The LWP backend uses pure Perl and persistent connections.
+
+This class does L<Search::Elasticsearch::Role::Cxn::HTTP>, whose documentation
+provides more information, L<Search::Elasticsearch::Role::Cxn> and
+L<Search::Elasticsearch::Role::Is_Sync>.
+
+=head1 CONFIGURATION
+
+=head2 Inherited configuration
+
+From L<Search::Elasticsearch::Role::Cxn::HTTP>
+
+=over
+
+=item * L<node|Search::Elasticsearch::Role::Cxn::HTTP/"node">
+
+=item * L<max_content_length|Search::Elasticsearch::Role::Cxn::HTTP/"max_content_length">
+
+=item * L<deflate|Search::Elasticsearch::Role::Cxn::HTTP/"deflate">
+
+=back
+
+From L<Search::Elasticsearch::Role::Cxn>
+
+=over
+
+=item * L<request_timeout|Search::Elasticsearch::Role::Cxn/"request_timeout">
+
+=item * L<ping_timeout|Search::Elasticsearch::Role::Cxn/"ping_timeout">
+
+=item * L<dead_timeout|Search::Elasticsearch::Role::Cxn/"dead_timeout">
+
+=item * L<max_dead_timeout|Search::Elasticsearch::Role::Cxn/"max_dead_timeout">
+
+=item * L<sniff_request_timeout|Search::Elasticsearch::Role::Cxn/"sniff_request_timeout">
+
+=item * L<sniff_timeout|Search::Elasticsearch::Role::Cxn/"sniff_timeout">
+
+=item * L<handle_args|Search::Elasticsearch::Role::Cxn/"handle_args">
+
+=back
+
+=head1 METHODS
+
+=head2 C<perform_request()>
+
+    ($status,$body) = $self->perform_request({
+        # required
+        method      => 'GET|HEAD|POST|PUT|DELETE',
+        path        => '/path/of/request',
+        qs          => \%query_string_params,
+
+        # optional
+        data        => $body_as_string,
+        mime_type   => 'application/json',
+        timeout     => $timeout
+    });
+
+Sends the request to the associated Elasticsearch node and returns
+a C<$status> code and the decoded response C<$body>, or throws an
+error if the request failed.
+
+=head2 Inherited methods
+
+From L<Search::Elasticsearch::Role::Cxn::HTTP>
+
+=over
+
+=item * L<scheme()|Search::Elasticsearch::Role::Cxn::HTTP/"scheme()">
+
+=item * L<is_https()|Search::Elasticsearch::Role::Cxn::HTTP/"is_https()">
+
+=item * L<userinfo()|Search::Elasticsearch::Role::Cxn::HTTP/"userinfo()">
+
+=item * L<default_headers()|Search::Elasticsearch::Role::Cxn::HTTP/"default_headers()">
+
+=item * L<max_content_length()|Search::Elasticsearch::Role::Cxn::HTTP/"max_content_length()">
+
+=item * L<build_uri()|Search::Elasticsearch::Role::Cxn::HTTP/"build_uri()">
+
+=back
+
+From L<Search::Elasticsearch::Role::Cxn>
+
+=over
+
+=item * L<host()|Search::Elasticsearch::Role::Cxn/"host()">
+
+=item * L<port()|Search::Elasticsearch::Role::Cxn/"port()">
+
+=item * L<uri()|Search::Elasticsearch::Role::Cxn/"uri()">
+
+=item * L<is_dead()|Search::Elasticsearch::Role::Cxn/"is_dead()">
+
+=item * L<is_live()|Search::Elasticsearch::Role::Cxn/"is_live()">
+
+=item * L<next_ping()|Search::Elasticsearch::Role::Cxn/"next_ping()">
+
+=item * L<ping_failures()|Search::Elasticsearch::Role::Cxn/"ping_failures()">
+
+=item * L<mark_dead()|Search::Elasticsearch::Role::Cxn/"mark_dead()">
+
+=item * L<mark_live()|Search::Elasticsearch::Role::Cxn/"mark_live()">
+
+=item * L<force_ping()|Search::Elasticsearch::Role::Cxn/"force_ping()">
+
+=item * L<pings_ok()|Search::Elasticsearch::Role::Cxn/"pings_ok()">
+
+=item * L<sniff()|Search::Elasticsearch::Role::Cxn/"sniff()">
+
+=item * L<process_response()|Search::Elasticsearch::Role::Cxn/"process_response()">
+
+=back
+
+=head1 SEE ALSO
+
+=over
+
+=item * L<Search::Elasticsearch::Role::Cxn::HTTP>
+
+=item * L<Search::Elasticsearch::Cxn::HTTPTiny>
+
+=item * L<Search::Elasticsearch::Cxn::Hijk>
+
+=item * L<Search::Elasticsearch::Cxn::NetCurl>
+
+=back
+
+=head1 AUTHOR
+
+Clinton Gormley <drtech@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2014 by Elasticsearch BV.
+
+This is free software, licensed under:
+
+  The Apache License, Version 2.0, January 2004
+
+=cut
---
+++ lib/Search/Elasticsearch/Role/Scroll.pm
@@ -0,0 +1,71 @@
+package Search::Elasticsearch::Role::Scroll;
+$Search::Elasticsearch::Role::Scroll::VERSION = '1.10';
+use Moo::Role;
+requires 'finish';
+use Search::Elasticsearch::Util qw(parse_params throw);
+use Scalar::Util qw(weaken blessed);
+use namespace::clean;
+
+has 'es' => ( is => 'ro', required => 1 );
+has 'scroll'        => ( is => 'ro' );
+has 'total'         => ( is => 'rwp' );
+has 'max_score'     => ( is => 'rwp' );
+has 'facets'        => ( is => 'rwp' );
+has 'aggregations'  => ( is => 'rwp' );
+has 'suggest'       => ( is => 'rwp' );
+has 'took'          => ( is => 'rwp' );
+has 'total_took'    => ( is => 'rwp' );
+has 'search_params' => ( is => 'ro' );
+has 'is_finished'   => ( is => 'rwp', default => '' );
+has '_scroll_id'    => ( is => 'rwp', clearer => 1, predicate => 1 );
+
+#===================================
+sub finish {
+#===================================
+    my $self = shift;
+    return if $self->is_finished;
+    $self->_set_is_finished(1);
+
+    my $scroll_id = $self->_scroll_id or return;
+    $self->_clear_scroll_id;
+    eval { $self->es->clear_scroll( scroll_id => $scroll_id ) };
+}
+
+#===================================
+sub DEMOLISH {
+#===================================
+    my $self = shift or return;
+    $self->finish;
+}
+
+1;
+
+# ABSTRACT: Provides common functionality to L<Elasticseach::Scroll> and L<Search::Elasticsearch::Async::Scroll>
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Search::Elasticsearch::Role::Scroll - Provides common functionality to L<Elasticseach::Scroll> and L<Search::Elasticsearch::Async::Scroll>
+
+=head1 VERSION
+
+version 1.10
+
+=head1 AUTHOR
+
+Clinton Gormley <drtech@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2014 by Elasticsearch BV.
+
+This is free software, licensed under:
+
+  The Apache License, Version 2.0, January 2004
+
+=cut
