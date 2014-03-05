2c5471d2ec52dc4c3d3666e832cdfbf66a215117
Clinton Gormley <develop@traveljury.com>
2014-03-05 15:16:33 +0000

    Import of DRTECH/ElasticSearch-0.67 from CPAN.
    gitpan-cpan-distribution: Search-Elasticsearch
    gitpan-cpan-version:      0.67
    gitpan-cpan-path:         DRTECH/ElasticSearch-0.67.tar.gz
    gitpan-cpan-author:       DRTECH
    gitpan-cpan-maturity:     released

---
+++ t/request_tests/version.pl
@@ -0,0 +1,20 @@
+#!perl
+
+use Test::More;
+use strict;
+use warnings;
+our ( $es, $es_version );
+my $r;
+
+isa_ok $r = $es->current_server_version, 'HASH', 'Current server version';
+ok $r->{number}, ' - has a version string';
+ok defined $r->{snapshot_build}, ' - has snapshot_build';
+
+note "Current server is "
+    . ( $r->{snapshot_build} ? 'development ' : '' )
+    . "version "
+    . $r->{number};
+
+$es_version = $r->{number};
+
+1;
